cf513a196a3ee58a8297da1dfbaa56d17fa74c36
Mordvintsevmv <mordvintsevmv@gmail.com>
2022-12-03 20:00:01 +0400

    Finishing Main Elements (version 1)

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,15 +5,22 @@ const Main = () => {
     return(
         <div className="main" style={{backgroundImage: `url(${process.env.PUBLIC_URL+ "/img/background.png"})`}}>
 
-            <h1>АКТИВИРУЙ ИММУНИТЕТ!</h1>
 
-            <p>Всего пять секунд в день помогут укрепить иммунитет
-                и повысить защитные силы организма</p>
+            <div className="tagline">
+                <h1 className="tagline__title">АКТИВИРУЙ ИММУНИТЕТ!</h1>
 
-            <h3>NOOTRIS ПОМОГАЕТ</h3>
+                <p className="tagline__description">Всего пять секунд в день помогут укрепить иммунитет
+                    и повысить защитные силы организма</p>
+            </div>
 
-            <p>Вашему организму во время пандемии
-                и сезонных простуд</p>
+
+
+            <div className="advantage">
+                <h3 className="advantage__title">NOOTRIS ПОМОГАЕТ</h3>
+
+                <p className="advantage__description">Вашему организму во время пандемии
+                    и сезонных простуд</p>
+            </div>
 
             <NootrisComponents/>
 
--- src/components/Main/NootrisComponents.jsx
+++ src/components/Main/NootrisComponents.jsx
@@ -1,9 +1,9 @@
 const NootrisComponents = () =>{
     return(
-        <div>
-            <img src={process.env.PUBLIC_URL+ "/img/nootris.png"} alt="Nootris"/>
-            <img src={process.env.PUBLIC_URL+ "/img/lemon.png"} alt="lemon"/>
-            <img src={process.env.PUBLIC_URL+ "/img/ginger.png"} alt="ginger"/>
+        <div className="nootrisimg">
+            <img className="nootrisimg__nootris" src={process.env.PUBLIC_URL+ "/img/nootris.png"} alt="Nootris"/>
+            <img className="nootrisimg__lemon" src={process.env.PUBLIC_URL+ "/img/lemon.png"} alt="lemon"/>
+            <img className="nootrisimg__ginger" src={process.env.PUBLIC_URL+ "/img/ginger.png"} alt="ginger"/>
         </div>
     )
 }
