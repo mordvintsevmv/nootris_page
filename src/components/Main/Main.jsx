8c2797744c0a0466e8d8978cc8efb0d5a6a0e7bd
Mordvintsevmv <mordvintsevmv@gmail.com>
2022-12-03 19:16:25 +0400

    Creating Main Elements

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,22 @@
 import "./Main.scss"
+import NootrisComponents from "./NootrisComponents";
 
 const Main = () => {
     return(
         <div className="main" style={{backgroundImage: `url(${process.env.PUBLIC_URL+ "/img/background.png"})`}}>
+
+            <h1>АКТИВИРУЙ ИММУНИТЕТ!</h1>
+
+            <p>Всего пять секунд в день помогут укрепить иммунитет
+                и повысить защитные силы организма</p>
+
+            <h3>NOOTRIS ПОМОГАЕТ</h3>
+
+            <p>Вашему организму во время пандемии
+                и сезонных простуд</p>
+
+            <NootrisComponents/>
+
         </div>
     )
 }
---
+++ src/components/Main/NootrisComponents.jsx
@@ -0,0 +1,11 @@
+const NootrisComponents = () =>{
+    return(
+        <div>
+            <img src={process.env.PUBLIC_URL+ "/img/nootris.png"} alt="Nootris"/>
+            <img src={process.env.PUBLIC_URL+ "/img/lemon.png"} alt="lemon"/>
+            <img src={process.env.PUBLIC_URL+ "/img/ginger.png"} alt="ginger"/>
+        </div>
+    )
+}
+
+export default NootrisComponents
